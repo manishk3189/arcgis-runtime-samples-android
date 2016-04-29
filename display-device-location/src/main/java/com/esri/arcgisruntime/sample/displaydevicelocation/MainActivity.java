faa5ccae0ea1144e1fdb867965b48f747cb4c409
Changan Shi <cshi@esri.com>
2016-04-29 14:43:50 -0700

    update the test of display device location

--- display-device-location/src/main/java/com/esri/arcgisruntime/sample/displaydevicelocation/MainActivity.java
+++ display-device-location/src/main/java/com/esri/arcgisruntime/sample/displaydevicelocation/MainActivity.java
@@ -18,12 +18,14 @@ package com.esri.arcgisruntime.sample.displaydevicelocation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
+import com.esri.arcgisruntime.location.LocationDataSource;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.Map;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.sample.spinner.ItemData;
@@ -33,107 +35,129 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MapView mMapView;
-    private LocationDisplay mLocationDisplay;
+  private MapView mMapView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private LocationDisplay mLocationDisplay;
 
-        Spinner spinner;
+  private boolean wasStarted = false;
 
-        // inflate MapView from layout
-        mMapView = (MapView) findViewById(R.id.mapView);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        // get the MapView's LocationDisplay
-        mLocationDisplay = mMapView.getLocationDisplay();
+    Spinner spinner;
 
-        // Populate the list for the Location display options for the spinner's Adapter
-        ArrayList<ItemData> list = new ArrayList<>();
-        list.add(new ItemData("Stop", R.drawable.locationdisplaydisabled));
-        list.add(new ItemData("On", R.drawable.locationdisplayon));
-        list.add(new ItemData("Re-Center", R.drawable.locationdisplayrecenter));
-        list.add(new ItemData("Navigation", R.drawable.locationdisplaynavigation));
-        list.add(new ItemData("Compass", R.drawable.locationdisplayheading));
+    // inflate MapView from layout
+    mMapView = (MapView) findViewById(R.id.mapView);
 
-        // inflate the Spinner from layout
-        spinner = (Spinner) findViewById(R.id.spinner);
-        SpinnerAdapter adapter = new SpinnerAdapter(this,
-                R.layout.spinner_layout, R.id.txt, list);
-        spinner.setAdapter(adapter);
+    // get the MapView's LocationDisplay
+    mLocationDisplay = mMapView.getLocationDisplay();
 
-        // create a map with the BasemapType Imagery
-        Map mMap = new Map(Basemap.createImagery());
-        // set the map to be displayed in this view
-        mMapView.setMap(mMap);
+    mLocationDisplay.addLocationChangedListener(new LocationDisplay.LocationChangedListener() {
+      @Override public void onLocationChanged(LocationDisplay.LocationChangedEvent event) {
+        LocationDataSource.Location loc = event.getLocation();
+        if (loc != null && loc.getPosition() != null) {
+          Log.d("location", "->" + event.getLocation().getPosition().getX() + ":" +
+              event.getLocation().getPosition().getY() + ":" +
+              event.getLocation().getVelocity()
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+          );
+        }
+      }
+    });
 
-                                           {
-                                               @Override
-                                               public void onItemSelected(AdapterView<?> parent, View view, int position,
-                                                                          long id) {
-                                                   switch (position) {
-                                                       case 0:
-                                                           // Stop Location Display
-                                                           if (mLocationDisplay.isStarted())
-                                                               mLocationDisplay.stop();
-                                                           break;
-                                                       case 1:
-                                                           // Start Location Display
-                                                           if (!mLocationDisplay.isStarted())
-                                                               mLocationDisplay.startAsync();
-                                                           break;
-                                                       case 2:
-                                                           // Re-Center MapView on Location
-                                                           // AutoPanMode - Default
-                                                           // In this mode, the MapView attempts to keep the location symbol on-screen by re-centering
-                                                           // the location symbol when the symbol moves outside a "wander extent." The location symbol
-                                                           // may move freely within the wander extent, but as soon as the symbol exits the wander extent,
-                                                           // the MapView re-centers the map on the symbol.
-                                                           mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.DEFAULT);
-                                                           if (!mLocationDisplay.isStarted())
-                                                               mLocationDisplay.startAsync();
-                                                           break;
-                                                       case 3:
-                                                           // Start Navigation Mode
-                                                           // This mode is best suited for in-vehicle navigation.
-                                                           mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.NAVIGATION);
-                                                           if (!mLocationDisplay.isStarted())
-                                                               mLocationDisplay.startAsync();
-                                                           break;
-                                                       case 4:
-                                                           // Start Compass Mode
-                                                           // This mode is better suited for waypoint navigation when the user is walking.
-                                                           mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS);
-                                                           if (!mLocationDisplay.isStarted())
-                                                               mLocationDisplay.startAsync();
-                                                           break;
-                                                   }
+    // Populate the list for the Location display options for the spinner's Adapter
+    ArrayList<ItemData> list = new ArrayList<>();
+    list.add(new ItemData("Stop", R.drawable.locationdisplaydisabled));
+    list.add(new ItemData("On", R.drawable.locationdisplayon));
+    list.add(new ItemData("Re-Center", R.drawable.locationdisplayrecenter));
+    list.add(new ItemData("Navigation", R.drawable.locationdisplaynavigation));
+    list.add(new ItemData("Compass", R.drawable.locationdisplayheading));
 
-                                               }
+    // inflate the Spinner from layout
+    spinner = (Spinner) findViewById(R.id.spinner);
+    SpinnerAdapter adapter = new SpinnerAdapter(this,
+        R.layout.spinner_layout, R.id.txt, list);
+    spinner.setAdapter(adapter);
 
-                                               @Override
-                                               public void onNothingSelected(AdapterView<?> parent) {
+    // create a map with the BasemapType Imagery
+    ArcGISMap mMap = new ArcGISMap(Basemap.createImagery());
+    // set the map to be displayed in this view
+    mMapView.setMap(mMap);
 
-                                               }
-                                           }
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
 
-        );
+      {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position,
+            long id) {
+          switch (position) {
+            case 0:
+              // Stop Location Display
+              if (mLocationDisplay.isStarted())
+                mLocationDisplay.stop();
+              break;
+            case 1:
+              // Start Location Display
+              if (!mLocationDisplay.isStarted())
+                mLocationDisplay.startAsync();
+              break;
+            case 2:
+              // Re-Center MapView on Location
+              // AutoPanMode - Default
+              // In this mode, the MapView attempts to keep the location symbol on-screen by re-centering
+              // the location symbol when the symbol moves outside a "wander extent." The location symbol
+              // may move freely within the wander extent, but as soon as the symbol exits the wander extent,
+              // the MapView re-centers the map on the symbol.
+              mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.DEFAULT);
+              if (!mLocationDisplay.isStarted())
+                mLocationDisplay.startAsync();
+              break;
+            case 3:
+              // Start Navigation Mode
+              // This mode is best suited for in-vehicle navigation.
+              mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.NAVIGATION);
+              if (!mLocationDisplay.isStarted())
+                mLocationDisplay.startAsync();
+              break;
+            case 4:
+              // Start Compass Mode
+              // This mode is better suited for waypoint navigation when the user is walking.
+              mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS);
+              if (!mLocationDisplay.isStarted())
+                mLocationDisplay.startAsync();
+              break;
+          }
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+      }
+
+    );
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mMapView.pause();
+    if (mLocationDisplay.isStarted()) {
+      mLocationDisplay.stop();
+      wasStarted = true;
     }
+  }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mMapView.pause();
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mMapView.resume();
+    if (wasStarted) {
+      mLocationDisplay.startAsync();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mMapView.resume();
-    }
-
+  }
 
 }
