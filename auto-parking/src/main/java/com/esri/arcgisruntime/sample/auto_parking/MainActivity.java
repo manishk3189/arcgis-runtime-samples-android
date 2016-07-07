e37b2237f1e7a800f2820a47a7f3060694965fa9
manishk3189 <khandelwal.manish31@gmail.com>
2016-07-06 17:50:29 -0700

    - custom floating searchview added

--- auto-parking/src/main/java/com/esri/arcgisruntime/sample/auto_parking/MainActivity.java
+++ auto-parking/src/main/java/com/esri/arcgisruntime/sample/auto_parking/MainActivity.java
@@ -1,6 +1,15 @@
 package com.esri.arcgisruntime.sample.auto_parking;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +18,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
-import com.esri.arcgisruntime.datasource.FeatureQueryResult;
-import com.esri.arcgisruntime.datasource.QueryParameters;
 import com.esri.arcgisruntime.datasource.arcgis.ArcGISFeature;
 import com.esri.arcgisruntime.datasource.arcgis.ServiceFeatureTable;
-import com.esri.arcgisruntime.geometry.Envelope;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.FeatureLayer;
@@ -22,6 +28,7 @@ import com.esri.arcgisruntime.mapping.GeoElement;
 import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.portal.Portal;
 import com.esri.arcgisruntime.portal.PortalItem;
@@ -37,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
     private PortalItem mPortalItem;
     private android.graphics.Point mClickPoint;
     private ArcGISFeature mSelectedArcGISFeature;
+    private int requestCode = 2;
+    String[] reqPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+            .ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private LocationDisplay mLocationDisplay;
+    FloatingActionButton mLocationFAB;
+    private AlertDialog.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
         // create a map with the BasemapType topographic
         //ArcGISMap mMap = new ArcGISMap(Basemap.createStreets());
 
+        // Build a alert dialog with specified style
+        builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
+
+        mLocationFAB = (FloatingActionButton) findViewById(R.id.locationFab);
         // get the portal url for ArcGIS Online
         mPortal = new Portal(getResources().getString(R.string.portal_url));
         // get the pre-defined portal id and portal url
@@ -69,9 +87,50 @@ public class MainActivity extends AppCompatActivity {
         // set the map to be displayed in the mapview
         mMapView.setMap(mMap);
 
-        //{"x":-13044550,"y":4040393,"spatialReference":{"wkid":102100}}
 
-        mMap.setInitialViewpoint(new Viewpoint(new Point(-13044550, 4040393, SpatialReferences.getWebMercator()), 5e4));
+
+        //{"x":-13044550,"y":4040393,"spatialReference":{"wkid":102100}} 13044550, 4040393
+
+        mMap.setInitialViewpoint(new Viewpoint(new Point(-117.195685, 34.056509, SpatialReferences.getWgs84()), 5e4));
+
+        // get the MapView's LocationDisplay
+        mLocationDisplay = mMapView.getLocationDisplay();
+
+        // Listen to changes in the status of the location data source.
+        mLocationDisplay.addDataSourceStatusChangedListener(new LocationDisplay.DataSourceStatusChangedListener() {
+            @Override
+            public void onStatusChanged(LocationDisplay.DataSourceStatusChangedEvent dataSourceStatusChangedEvent) {
+
+                // If LocationDisplay started OK, then continue.
+                if (dataSourceStatusChangedEvent.isStarted())
+                    return;
+
+                // No error is reported, then continue.
+                if (dataSourceStatusChangedEvent.getSource().getLocationDataSource().getError() == null)
+                    return;
+
+                // If an error is found, handle the failure to start.
+                // Check permissions to see if failure may be due to lack of permissions.
+                boolean permissionCheck1 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermissions[0]) ==
+                        PackageManager.PERMISSION_GRANTED;
+                boolean permissionCheck2 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermissions[1]) ==
+                        PackageManager.PERMISSION_GRANTED;
+
+                if (!(permissionCheck1 && permissionCheck2)) {
+                    // If permissions are not already granted, request permission from the user.
+                    ActivityCompat.requestPermissions(MainActivity.this, reqPermissions, requestCode);
+                } else {
+                    // Report other unknown failure types to the user - for example, location services may not
+                    // be enabled on the device.
+                    String message = String.format("Error in DataSourceStatusChangedListener: %s", dataSourceStatusChangedEvent
+                            .getSource().getLocationDataSource().getError().getMessage());
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+                    buildAlertMessageNoGps();
+
+                }
+            }
+        });
 
 
         // set an on touch listener to listen for click events
@@ -114,6 +173,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mLocationFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!mLocationDisplay.isStarted()) {
+                    mLocationDisplay.startAsync();
+                }
+                if(mLocationDisplay.isStarted()) {
+                    Log.d(TAG,"showLocation");
+                    Point currentLocation = mLocationDisplay.getMapLocation();
+                    Viewpoint vp = new Viewpoint(currentLocation, 5e3);
+                    mMapView.setViewpointWithDurationAsync(vp, 3);
+                }
+            }
+        });
+
+
 
     }
 
@@ -128,4 +203,34 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mMapView.resume();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Location permission was granted. This would have been triggered in response to failing to start the
+            // LocationDisplay, so try starting this again.
+            mLocationDisplay.startAsync();
+        } else {
+            // If permission was denied, show toast to inform user what was chosen. If LocationDisplay is started again,
+            // request permission UX will be shown again, option should be shown to allow never showing the UX again.
+            // Alternative would be to disable functionality so request is not shown again.
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied), Toast
+                    .LENGTH_SHORT).show();
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please enable your GPS before proceeding")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 }
